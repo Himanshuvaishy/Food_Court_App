bf9f25b5243a00e16df4faf2b8313a96f2eb4c80
Himanshu <vaishhimanshu83170@gmail.com>
2024-05-13 09:45:59 -1100

    all code update in episode 2

--- NamasteReact_/App.js
+++ NamasteReact_/App.js
@@ -1,6 +1,12 @@
-const heading=React.createElement("h1",{id:"heading"},"Hlo React");
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading=React.createElement("h1",{id:"heading"},"I am react app");
 console.log(heading)  
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
+  
