de227fe2767ed5ecfa1b0eb38b41e1236b6e3437
Himanshu <vaishhimanshu83170@gmail.com>
2024-05-13 11:41:33 -1100

    add

---
+++ App.js
@@ -0,0 +1,10 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading=React.createElement("h1",{id:"heading"},"I am react app");
+console.log(heading)  
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
