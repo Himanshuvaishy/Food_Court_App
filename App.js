f1d6e22455109207fa58eeb502165f3d204af78d
Himanshu <vaishhimanshu83170@gmail.com>
2024-05-19 17:45:29 -1100

    add some new file

--- App.js
+++ App.js
@@ -1,39 +1,45 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React element ---> object==>Html element by usingrendor mrethod
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCckSuxKU7kmm48EhK2vOJebP1_HY0e9d7dRSVDs2ebA&s"
+                />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li> Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+
+    )
+}
+
+
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+
+        </div>
+    )
+}
 
 
 
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// Jxs
-let Title= ()=>(
-<>
-<h1 id="heading">Hlo  react</h1>
-</>
-)
-
-// react componenet
-
-const Headingcomponent =  function (){
-    return ( <div>
-        <Title/>
-        {Title()}
-        <Title></Title>
-        <h1> Namaste react</h1>
-        <h2>Learning react is not tough if u have teacher like Akshay</h2>
-    </div>
-)}
-
-
-
-
-
-const root= ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<Headingcomponent/>);
+root.render(<AppLayout />);
 
 
 
